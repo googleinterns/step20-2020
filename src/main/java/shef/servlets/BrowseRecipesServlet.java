86d7c7079fd3346584589c382902f9c9579afe8d
Tommy Gomez <tomgom@google.com>
2020-07-10 11:43:14 -0700

    Update import statements

--- src/main/java/shef/servlets/BrowseRecipesServlet.java
+++ src/main/java/shef/servlets/BrowseRecipesServlet.java
@@ -23,6 +23,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import shef.data.Recipe;
 import shef.data.RecipeFilter;
