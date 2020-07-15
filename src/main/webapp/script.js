26ccbcd01396dd4a671b1ebec7328c8e7bbf9524
Tommy Gomez <tomgom@google.com>
2020-07-15 19:41:32 +0000

    add loading methods to javascript

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,6 +1,6 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -37,4 +37,32 @@ function createCommentElement(comment) {
 
 function addParagraph(content) {
   return "<p>" + content + "</p>";
+}
+
+function newGroupchat() {
+  const request = new Request("/load-groupchat", {method: 'POST'});
+  fetch(request).then(response => response.text()).then((key) => {
+    redirectToGroupchat(key);
+  });
+}
+
+function loadGroupchat() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const key = urlParams.get('key');
+  fetch('/load-groupchat?key=' + key).then(response => response.json()).then((messages) => {
+    const messageContainer = document.getElementById('messages');
+    messageContainer.innerHTML = '';
+    for (var i = 0; i < messages.length; i++) {
+      const message = document.createElement('p');
+      message.innerText = messages[i];
+      messageContainer.appendChild(message);
+    }
+  }).catch(error =>
+    alert('Error: Groupchat does not exist')
+  );
+}
+
+function redirectToGroupchat(keyParameter) {
+  const key = keyParameter ? keyParameter : document.getElementById('groupchat-key').value;
+  window.location.href = "/groupchat.html?key=" + key;
 }
