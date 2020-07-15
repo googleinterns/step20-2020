451b4a899c228752d0b77289a3cf39b34a41438f
Tommy Gomez <tomgom@google.com>
2020-07-15 19:00:33 +0000

    create servlet to retrieve and post groupchats

--- src/main/java/shef/servlets/GroupchatServlet.java
+++ src/main/java/shef/servlets/GroupchatServlet.java
@@ -17,23 +17,53 @@ package shef.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
-@WebServlet("/groupchat")
+@WebServlet("/load-groupchat")
 public class GroupchatServlet extends HttpServlet {
+
+  DatastoreService datastore;
+
+  @Override
+  public void init() {
+    datastore = DatastoreServiceFactory.getDatastoreService();
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String keyString = request.getParameter("key");
+    Entity groupchatEntity = null;
+    try {
+      groupchatEntity = datastore.get(KeyFactory.stringToKey(keyString));
+    } catch (EntityNotFoundException e) {
+      e.printStackTrace();
+      return;
+    }
 
+    ArrayList<String> messages = (ArrayList<String>) groupchatEntity.getProperty("messages");
+    response.setContentType("application/json;");
+    Gson gson = new Gson();
+    response.getWriter().println(gson.toJson(messages));
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
+    Entity groupchat = new Entity("Groupchat");
+    groupchat.setProperty("messages", new ArrayList<String>());
+    datastore.put(groupchat);
+
+    response.setContentType("text/html");
+    String groupchatKey = KeyFactory.keyToString(groupchat.getKey());
+    response.getWriter().println(groupchatKey);  
   }
 
 }
