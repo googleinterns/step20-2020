e0f73dc66cb7425f0b52ab4d7b27821c031ccf52
Tommy Gomez <tomgom@google.com>
2020-07-17 00:32:47 +0000

    add Groupchat class for auxiliary methods

---
+++ src/main/java/shef/data/Groupchat.java
@@ -0,0 +1,48 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shef.data;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+
+public class Groupchat {
+
+  private ArrayList<String> messages;
+
+  public Groupchat(String key) {
+    Entity entity = null;
+    try {
+      entity = datastore.get(KeyFactory.stringToKey(key));
+    } catch (EntityNotFoundException e) {
+      e.printStackTrace();
+      return;
+    }
+    this(entity);
+  }
+
+  public Groupchat(Entity entity) {
+    Object messageObject = entity.getProperty("messages");
+    ArrayList<String> messages = messageObject != null ? (ArrayList<String>) messageObject : new ArrayList<>();
+  }
+
+  public ArrayList<String> getMessages() {
+    return messages;
+  }
+
+}
